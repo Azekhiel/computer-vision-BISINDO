219a9a728256caca4e767c249627bc28339ac9ae
Azekhiel <williambriandelo@gmail.com>
2026-04-28 11:37:38 +0700

    feat: feature extraction relative ke hidung

--- src/feature_engine.py
+++ src/feature_engine.py
@@ -3,64 +3,61 @@ import numpy as np
 def extract_keypoints_relative(results):
     """
     Mengekstrak tepat 144-dimensi fitur spasial dari MediaPipe Holistic.
-    Koordinat dibuat relatif terhadap hidung untuk menghilangkan bias posisi kamera.
+    DIPERBAIKI:
+    - Pose dihitung relatif terhadap Hidung.
+    - Tangan Kiri dihitung relatif terhadap Pergelangan Tangan Kiri.
+    - Tangan Kanan dihitung relatif terhadap Pergelangan Tangan Kanan.
+    Ini mengisolasi 'Bentuk Tangan' dari 'Posisi Tangan'.
     """
-    # Set default anchor di tengah jika pose tubuh tidak terdeteksi
-    anchor_x, anchor_y, anchor_z = 0.5, 0.5, 0.0 
     
+    # 1. POSE (Relatif ke Hidung)
+    pose_coords = []
     if results.pose_landmarks:
-        # Gunakan hidung (landmark 0) sebagai titik pusat (0,0,0)
-        anchor_x = results.pose_landmarks.landmark[0].x
-        anchor_y = results.pose_landmarks.landmark[0].y
-        anchor_z = results.pose_landmarks.landmark[0].z
-
-    def get_relative_coords(landmarks, num_points, specific_indices=None):
-        if not landmarks:
-            if specific_indices:
-                return np.zeros(len(specific_indices) * 3)
-            return np.zeros(num_points * 3)
+        # Hidung adalah landmark 0
+        nose_x = results.pose_landmarks.landmark[0].x
+        nose_y = results.pose_landmarks.landmark[0].y
+        nose_z = results.pose_landmarks.landmark[0].z
         
-        rel_coords = []
-        if specific_indices:
-            # Hanya ekstrak titik tertentu (untuk pose tubuh)
-            for idx in specific_indices:
-                res = landmarks.landmark[idx]
-                rel_coords.extend([
-                    res.x - anchor_x, 
-                    res.y - anchor_y, 
-                    res.z - anchor_z
-                ])
-        else:
-            # Ekstrak semua titik (untuk jari tangan)
-            for res in landmarks.landmark:
-                rel_coords.extend([
-                    res.x - anchor_x, 
-                    res.y - anchor_y, 
-                    res.z - anchor_z
-                ])
-        return rel_coords
+        pose_indices = [11, 12, 13, 14, 15, 16] # Bahu, Siku, Pergelangan
+        for idx in pose_indices:
+            res = results.pose_landmarks.landmark[idx]
+            pose_coords.extend([res.x - nose_x, res.y - nose_y, res.z - nose_z])
+    else:
+        pose_coords = list(np.zeros(18)) # 6 titik x 3 dimensi
 
-    # 1. Ekstrak 6 titik Pose (Bahu Kiri-Kanan, Siku Kiri-Kanan, Pergelangan Kiri-Kanan)
-    # 6 titik * 3 (X, Y, Z) = 18 Dimensi
-    pose_indices = [11, 12, 13, 14, 15, 16]
-    pose = get_relative_coords(results.pose_landmarks, 33, specific_indices=pose_indices)
-    
-    # 2. Ekstrak 21 titik Tangan Kiri
-    # 21 titik * 3 (X, Y, Z) = 63 Dimensi
-    lh = get_relative_coords(results.left_hand_landmarks, 21)
-    
-    # 3. Ekstrak 21 titik Tangan Kanan
-    # 21 titik * 3 (X, Y, Z) = 63 Dimensi
-    rh = get_relative_coords(results.right_hand_landmarks, 21)
-    
+    # 2. TANGAN KIRI (Relatif ke Pergelangan Tangan Kiri)
+    lh_coords = []
+    if results.left_hand_landmarks:
+        # Wrist adalah landmark 0 dari tangan
+        wrist_x = results.left_hand_landmarks.landmark[0].x
+        wrist_y = results.left_hand_landmarks.landmark[0].y
+        wrist_z = results.left_hand_landmarks.landmark[0].z
+        
+        for res in results.left_hand_landmarks.landmark:
+            lh_coords.extend([res.x - wrist_x, res.y - wrist_y, res.z - wrist_z])
+    else:
+        lh_coords = list(np.zeros(63)) # 21 titik x 3 dimensi
+
+    # 3. TANGAN KANAN (Relatif ke Pergelangan Tangan Kanan)
+    rh_coords = []
+    if results.right_hand_landmarks:
+        # Wrist adalah landmark 0 dari tangan
+        wrist_x = results.right_hand_landmarks.landmark[0].x
+        wrist_y = results.right_hand_landmarks.landmark[0].y
+        wrist_z = results.right_hand_landmarks.landmark[0].z
+        
+        for res in results.right_hand_landmarks.landmark:
+            rh_coords.extend([res.x - wrist_x, res.y - wrist_y, res.z - wrist_z])
+    else:
+        rh_coords = list(np.zeros(63)) # 21 titik x 3 dimensi
+        
     # Total Vektor: 18 + 63 + 63 = 144 Dimensi
-    return np.concatenate([pose, lh, rh])
+    return np.array(pose_coords + lh_coords + rh_coords)
 
 def calculate_movement_score(prev_vector, curr_vector):
     """
     Menghitung jarak L2 Norm dari vektor 144-D antar 2 frame berurutan.
-    Fungsi ini menjadi inti dari sistem Visual Activity Detection (VAD) 
-    untuk mendeteksi kapan isyarat dimulai dan kapan tangan kembali diam.
+    Fungsi ini menjadi inti dari sistem Visual Activity Detection (VAD).
     """
     if prev_vector is None or curr_vector is None:
         return 0.0
